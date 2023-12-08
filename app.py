bf36cf69a62c9e1ebb69ceeb48226cfef567590f
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-12-07 21:39:02 -0500

    change to use postgresql

--- app.py
+++ app.py
@@ -2,33 +2,34 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:kl;\'@localhost/flask-project'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
-class Item(db.Model):
+class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
-@app.route('/add_item', methods=['POST'])
-def add_item():
+@app.route('/add_data', methods=['POST'])
+def add_data():
     data = request.get_json()
-    new_item = Item(name=data['name'])
 
-    try:
-        db.session.add(new_item)
-        db.session.commit()
-        return jsonify({'message': 'Item added successfully!'}), 201
-    except:
-        return jsonify({'message': 'Error adding item.'}), 500
+    new_data = Data(name=data['name'])
 
-@app.route('/get_items', methods=['GET'])
-def get_items():
-    items = Item.query.all()
-    item_list = []
-    for item in items:
-        item_list.append({'name': item.name})
-    return jsonify({'items': item_list})
+    db.session.add(new_data)
+    db.session.commit()
+
+    return jsonify({'message': 'Data added successfully'}), 201
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    data = Data.query.all()
+    data_list = [{'id': item.id, 'name': item.name} for item in data]
+
+    return jsonify({'data': data_list})
 
 if __name__ == '__main__':
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
